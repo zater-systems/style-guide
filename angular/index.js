64b463f9e96d86fc609d8e255ae23335b38445ba
Nathan Mc Grath <contact@nathan-mcgrath.com>
2018-06-18 19:04:25 +0100

    add initial typescript and angular style guide configs

---
+++ angular/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  rulesDirectory: ['codelyzer'],
+  extends: ['../typescript/index.js', 'tslint-angular'],
+};
---
+++ typescript/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['tslint:all'],
+  rules: {
+    'member-access': true,
+    typedef: [true, 'call-signature', 'arrow-call-signature'],
+    'only-arrow-functions': false,
+    'no-floating-promises': false,
+    'no-unsafe-any': false,
+    'no-submodule-imports': false,
+    'max-classes-per-file': false,
+    'newline-per-chained-call': false,
+    'prefer-function-over-method': false,
+    'completed-docs': false,
+    'no-parameter-properties': false,
+    'no-unnecessary-class': false,
+    'no-import-side-effect': false,
+    'no-implicit-dependencies': false,
+    'arrow-parens': false,
+  },
+};
